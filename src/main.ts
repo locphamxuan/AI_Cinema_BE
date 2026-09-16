bf2897598cae413c4169a72ff05ff591f63af236
manhhiep <doanmanhhiep.2003@gmail.com>
2026-09-16 21:52:04 +0700

    feat: integrate Swagger for API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('AI Cinema API')
+    .setDescription('AI Cinema - A Coin-Based Streaming System for AI-Generated Movies')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
